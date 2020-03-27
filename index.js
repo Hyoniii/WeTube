abfe51d3f984134177caf0065777d007178256f5
Hyoniii <phhphh841@hanmail.net>
2020-03-27 11:59:44 +0900

    hh

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const express = require("express");
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
+const handleListening = () =>
   console.log(`listening on: http://localhost:${PORT}`);
-}
 
-function handleHome(req, res) {
+const handleHome = (req, res) => {
   console.log(req);
-  res.send("hello from home");
-}
+  return res.send("hello from ass");
+};
 
-function handleProfile(req, res) {
-  res.send("You are on my profile");
-}
+const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
