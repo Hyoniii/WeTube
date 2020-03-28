aaf60be3a3d42903de2062f144b417eeb69931e0
Hyoniii <phhphh841@hanmail.net>
2020-03-28 22:46:55 +0900

    commit

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
-
-const handleHome = (req, res) => res.send("hello from ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,10 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); //보안을 위한 middleware
 app.use(morgan("dev")); //logging
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app; //이 파일을 import할 때 app object를 주겠다는 뜻. app object는 이미 설정한 코드들(#13-#21).
