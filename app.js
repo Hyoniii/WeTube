c1271e05619e9f267089aeb20ffda6c31bc5542b
Hyoniii <phhphh841@hanmail.net>
2020-03-29 18:23:01 +0900

    pug

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
+import { localsMiddlewares } from "./middlewares";
 
 const app = express();
 
+app.use(helmet()); //보안을 위한 middleware
+app.set("view engine", "pug"); //view engine을 undefined(default값)에서 pug로 지정해주는 코드.
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet()); //보안을 위한 middleware
 app.use(morgan("dev")); //logging
 
+app.use(localsMiddlewares);
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,14 @@
 //global routes
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
+export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+export const login = (req, res) => res.render("Login", { pageTitle: "Log In" });
+export const logout = (req, res) =>
+  res.render("Logout", { pageTitle: "Log Out" });
 
 //users routes
-export const users = (req, res) => res.send("Users");
-export const userDetail = (req, res) => res.send("User Detail");
-export const editProfile = (req, res) => res.send("Edit Profile");
-export const changePassword = (req, res) => res.send("Change Password");
+export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+export const userDetail = (req, res) =>
+  res.render("User Detail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.render("Edit Profile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.render("Change Password", { pageTitle: "Chang Password" });
