27e6ac15f605077938eab727716d18a9e6f55a18
Hyoniii <phhphh841@hanmail.net>
2020-03-29 13:56:58 +0900

    exprss - controller ing

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 
@@ -15,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); //보안을 위한 middleware
 app.use(morgan("dev")); //logging
 
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app; //이 파일을 import할 때 app object를 주겠다는 뜻. app object는 이미 설정한 코드들(#13-#21).
