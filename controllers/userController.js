744dd3e27817ba2a519d8d746a9b56dfaf879eb4
Hyoniii <phhphh841@hanmail.net>
2020-03-30 18:16:35 +0900

    pug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,14 @@
 //global routes
 export const join = (req, res) => res.render("join", { pageTitle: "Join" });
-export const login = (req, res) => res.render("Login", { pageTitle: "Log In" });
+export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
 export const logout = (req, res) =>
-  res.render("Logout", { pageTitle: "Log Out" });
+  res.render("logout", { pageTitle: "Log Out" });
 
 //users routes
-export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
+export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) =>
-  res.render("User Detail", { pageTitle: "User Detail" });
+  res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) =>
-  res.render("Edit Profile", { pageTitle: "Edit Profile" });
+  res.render("editProfile", { pageTitle: "Edit Profile" });
 export const changePassword = (req, res) =>
-  res.render("Change Password", { pageTitle: "Chang Password" });
+  res.render("changePassword", { pageTitle: "Chang Password" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,16 +1,21 @@
 // golbal routes
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("Search", { pageTitle: "Search" });
 
+export const search = (req, res) => {
+  //ECMA -> const searchingBy = req.search.term
+  const {
+    query: { term: searchingBy }
+  } = req;
+  res.render("search", { pageTitle: "Search", searchingBy }); //seachingBy : searchingBy 와 같은 문법.ES6에서는 자동
+};
 //video routes
 export const videos = (req, res) =>
-  res.render("Videos", { pageTitle: "Videos" });
+  res.render("videos", { pageTitle: "Videos" });
 export const upload = (req, res) =>
-  res.render("Upload", { pageTitle: "Upload" });
+  res.render("upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) =>
-  res.render("Video Detail", { pageTitle: "Video Detail" });
+  res.render("videoDetail", { pageTitle: "Video Detail" });
 export const editVideo = (req, res) =>
-  res.render("Edit Video", { pageTitle: "Edit Video" });
+  res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideo = (req, res) =>
-  res.render("Delete Video", { pageTitle: "Delete Video" });
+  res.render("deleteVideo", { pageTitle: "Delete Video" });
