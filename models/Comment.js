f9a644601e29b94c2aecce35d28e920ad30af0dc
Hyoniii <phhphh841@hanmail.net>
2020-04-02 23:15:09 +0900

    mongoose model

--- models/Comment.js
+++ models/Comment.js
@@ -13,3 +13,4 @@ const CommentSchima = new mongoose.Schema({
 
 const model = mongoose.model("Comment", CommentSchima);
 export default model;
+//새 JS로 작업하고 모듈을 사용하기 때문에 모델을 기본값으로 내보내고 나중에 주석 또는 사용자로 가져 오기 때문에 변수 이름이 동일한 지 여부는 중요하지 않습니다.
