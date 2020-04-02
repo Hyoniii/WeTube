2d350f8d53f646aa648e03f0a1bc6bd4b5cc9430
Hyoniii <phhphh841@hanmail.net>
2020-04-02 21:11:45 +0900

    Video model

---
+++ models/Video.js
@@ -0,0 +1,28 @@
+//MongDB에게 내 video들의 형태를 정의해서 알려주는 파일.
+//model(document name)과 schema(형태) 정의
+//video를 database에 넣지않는다. bytes가 아닌 link를 넣어준다.
+
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  fileUrl: {
+    type: String,
+    required: "File URL is required"
+  },
+  title: {
+    type: String,
+    required: "File URL is required"
+  },
+  description: String,
+  views: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now //mongoose가 자동으로 () 실행
+  }
+});
+
+const model = mongoose.model(); //("Video",VideoSchema);
+export default model;
